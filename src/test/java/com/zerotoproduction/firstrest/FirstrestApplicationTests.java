751005234df4ec1b19948d1930835d5ec967269c
Abraham Carrera Groba <abraham.carreragrom@alumnos.upm.es>
2023-09-02 21:16:32 +0200

    first commit

---
+++ src/test/java/com/zerotoproduction/firstrest/FirstrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerotoproduction.firstrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
